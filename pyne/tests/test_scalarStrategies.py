4e3ddae0cf15493163627ef35f273307b5f74752
François Trahan <ftrahan@enerosolutions.com>
2017-12-31 11:02:54 -0500

    Test for stuff

--- pyne/tests/test_scalarStrategies.py
+++ pyne/tests/test_scalarStrategies.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
-from pyne import Decision
 import pyne.strategies as strategies
+from pyne import Decision
 
 
 
@@ -14,17 +14,6 @@ class TestScalarStrategies(TestCase):
         root.propagatePayouts(0)
         self.root = root
 
-    def test_maxExpected(self):
-
-        EXPECTED = [(0.1, -500), (0.9, 0)]
-        CRITERIA = strategies.maxExpectedPayout
-        EXPECTED_STRATEGY = ['Do I buid something ?: No']
-
-        result = self.root.computePossibilities(CRITERIA)
-
-        self.assertEqual(EXPECTED, result)
-        self.assertEqual(EXPECTED_STRATEGY, self.getStrategy())
-
 
     def getStrategy(self):
         nodes = self.root.getNodesFlat()
@@ -34,5 +23,56 @@ class TestScalarStrategies(TestCase):
         return ["{}: {}".format(d.name, d.choice.name) for d in decisions]
 
 
+    def test_maxExpected(self):
+        EXPECTED = [(0.1, -500), (0.9, 0)]
+        CRITERIA = strategies.maxExpectedPayout
+        EXPECTED_STRATEGY = ['Do I buid something ?: No']
+
+        result = self.root.computePossibilities(CRITERIA)
+
+        self.assertEqual(EXPECTED, result)
+        self.assertEqual(EXPECTED_STRATEGY, self.getStrategy())
+
+    def test_maxMax(self):
+        EXPECTED = [(0.1, -500), (0.9, 0)]
+        CRITERIA = strategies.maxMaxPayout
+        EXPECTED_STRATEGY = ['Do I buid something ?: No']
+
+        result = self.root.computePossibilities(CRITERIA)
+
+        self.assertEqual(EXPECTED, result)
+        self.assertEqual(EXPECTED_STRATEGY, self.getStrategy())
+
+
+    def test_minMin(self):
+        EXPECTED = [(0.1, -500), (0.9, 0)]
+        CRITERIA = strategies.minMinPayout
+        EXPECTED_STRATEGY = ['Do I buid something ?: No']
+
+        result = self.root.computePossibilities(CRITERIA)
+
+        self.assertEqual(EXPECTED, result)
+        self.assertEqual(EXPECTED_STRATEGY, self.getStrategy())
+
+
+    def test_maxMin(self):
+        EXPECTED = [(1, -100)]
+        CRITERIA = strategies.maxMinPayout
+        EXPECTED_STRATEGY = ['Do I buid something ?: Yes']
+
+        result = self.root.computePossibilities(CRITERIA)
+
+        self.assertSequenceEqual(EXPECTED, result)
+        self.assertEqual(EXPECTED_STRATEGY, self.getStrategy())
+
+    def test_minMax(self):
+        EXPECTED = ((1, -100),)
+        CRITERIA = strategies.minMaxPayout
+        EXPECTED_STRATEGY = ['Do I buid something ?: Yes']
+
+        result = self.root.computePossibilities(CRITERIA)
+
+        self.assertEqual(EXPECTED, result)
+        self.assertEqual(EXPECTED_STRATEGY, self.getStrategy())
 
 from . import buildOrNotTestTree
