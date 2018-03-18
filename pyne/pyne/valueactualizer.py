5bb4de0ff229471bcb787e2098a72b742fee2a0a
François Trahan <ftrahan@enerosolutions.com>
2018-03-18 10:31:54 -0400

    TimeToRecoveryActualizer

--- pyne/pyne/valueactualizer.py
+++ pyne/pyne/valueactualizer.py
@@ -32,5 +32,48 @@ class SummingActualizer(ValueActualizer):
 
 
 
+class TimeToRecoveryActualizer(ValueActualizer):
+
+    def actualize(self, cashflow: pd.Series) -> Real:
+        assert isinstance(cashflow, pd.Series)
+
+        cashflow = cashflow.expanding().sum()
+
+        cfiter = iter(cashflow.iteritems())
+
+        # Still positive
+        try:
+            while True:
+                period, amount = next(cfiter)
+                if amount < 0:  # switching negative
+                    firstNegativePeriod = period
+                    break
+        except StopIteration:
+            return 0
+
+        # Went under zero
+        try:
+            while True:
+                period, amount = next(cfiter)
+                if amount >= 0:  # back to positive
+                    breakEvenPeriod = period
+                    break
+        except StopIteration:
+            return None
+
+        # Back in black
+        try:
+            while True:
+                period, amount = next(cfiter)
+                if amount < 0: # Going under zero for a second time: cannot evaluate
+                    return None
+        except StopIteration:
+            return breakEvenPeriod - firstNegativePeriod
+
+        # Done
+
+
+
 SCALAR_ACTUALIZER = ScalarActualizer()
 SUMMING_ACTUALIZER = SummingActualizer()
+TTR_ACTUALIZER = TimeToRecoveryActualizer()
