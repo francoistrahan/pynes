216c2be2ecd3ebf1842a154a8db194622ad91641
François Trahan <ftrahan@enerosolutions.com>
2018-03-20 14:32:16 -0400

    testcase for cashflow format

--- pyne/tests/test_draw.py
+++ pyne/tests/test_draw.py
@@ -1,9 +1,13 @@
 from unittest import TestCase, skip
 
-from pyne import Solver
+import pandas as pd
+
+import pyne.cashflow
+from pyne import *
 from pyne.render import GraphvizEngine
 from pyne.strategy import *
-from . import createMineralsSampleTreeScalar, IGNORE_DIRECTORY
+from pyne.valueactualizer import PeriodNPV
+from . import createMineralsSampleTreeScalar, IGNORE_DIRECTORY, showTree
 
 
 
@@ -47,3 +51,29 @@ class TestDraw(TestCase):
         filename = "Minerals - {} - {}".format(name, prune and "pruned" or "complete")
 
         graph.render(view=True, directory=IGNORE_DIRECTORY, filename=filename)
+
+
+    def test_cashflows(self):
+        CF = pyne.cashflow.createMonths
+        AN = pyne.cashflow.annuityMonths
+
+        mbaPattern = Event("Pass First Year", [
+            Transition("No", probability=0.1),
+            Transition("Yes", CF({"2017-1":-20000, "2017-07":-20000}), target=Event("Get a better Job", [
+                Transition("No", probability=.2),
+                Transition("Yes", AN("2018-01", 12 * 10, 20000 / 12))
+            ]))
+        ])
+
+        root = Decision("Do an MBA", [
+            Transition("No"),
+            Transition("Yes", CF({"2016-1":-20000, "2016-07":-20000}), target=Event("Get a Grant", [
+                Transition("No", target=mbaPattern.clone()),
+                Transition("Yes", CF({"2015-12":10000}), .2, mbaPattern.clone())
+            ]))
+        ])
+
+        solver = Solver(root, createMaxExpected(), PeriodNPV(0.1 / 12, pd.Period("2015-12", "M")))
+        solver.solve()
+
+        showTree(root)
