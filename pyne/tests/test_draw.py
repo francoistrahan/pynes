1b4490f840b9347ba710ee1faec2b332349224b4
François Trahan <ftrahan@enerosolutions.com>
2018-03-20 14:54:03 -0400

    formatSerie dans GraphvisEngine

--- pyne/tests/test_draw.py
+++ pyne/tests/test_draw.py
@@ -6,7 +6,7 @@ import pyne.cashflow
 from pyne import *
 from pyne.render import GraphvizEngine
 from pyne.strategy import *
-from pyne.valueactualizer import PeriodNPV
+from pyne.valueactualizer import PeriodNPV, IndexedNPV
 from . import createMineralsSampleTreeScalar, IGNORE_DIRECTORY, showTree
 
 
@@ -53,27 +53,59 @@ class TestDraw(TestCase):
         graph.render(view=True, directory=IGNORE_DIRECTORY, filename=filename)
 
 
-    def test_cashflows(self):
+    def test_cashflowsIndex(self):
+        CF = pyne.cashflow.create
+        AN = pyne.cashflow.indexAnnuity
+
+        mbaPattern = Event("Pass First Year", [
+            Transition("No", probability=0.1),
+            Transition("Yes", CF({13:-20000, 19:-20000}), target=Event("Pass Second Year", [
+                Transition("No", probability=.1),
+                Transition("Yes", target=Event("Get a better Job", [
+                    Transition("No", probability=.2),
+                    Transition("Yes", AN(25, 12 * 10, 20000 / 12))
+                ]))
+            ]))
+        ])
+
+        root = Decision("Do an MBA (index)", [
+            Transition("No"),
+            Transition("Yes", CF({1:-20000, 7:-20000}), target=Event("Get a Grant", [
+                Transition("No", target=mbaPattern.clone()),
+                Transition("Yes", CF({0:10000}), .5, mbaPattern.clone())
+            ]))
+        ])
+
+        solver = Solver(root, createMaxExpected(), IndexedNPV(10 / 100 / 12))
+        solver.solve()
+
+        showTree(root, "${:,.2f}")
+
+
+    def test_cashflowsPeriod(self):
         CF = pyne.cashflow.createMonths
         AN = pyne.cashflow.annuityMonths
 
         mbaPattern = Event("Pass First Year", [
             Transition("No", probability=0.1),
-            Transition("Yes", CF({"2017-1":-20000, "2017-07":-20000}), target=Event("Get a better Job", [
-                Transition("No", probability=.2),
-                Transition("Yes", AN("2018-01", 12 * 10, 20000 / 12))
+            Transition("Yes", CF({"2017-1":-20000, "2017-07":-20000}), target=Event("Pass Second Year", [
+                Transition("No", probability=.1),
+                Transition("Yes", target=Event("Get a better Job", [
+                    Transition("No", probability=.2),
+                    Transition("Yes", AN("2018-01", 12 * 10, 20000 / 12))
+                ]))
             ]))
         ])
 
-        root = Decision("Do an MBA", [
+        root = Decision("Do an MBA (periods)", [
             Transition("No"),
             Transition("Yes", CF({"2016-1":-20000, "2016-07":-20000}), target=Event("Get a Grant", [
                 Transition("No", target=mbaPattern.clone()),
-                Transition("Yes", CF({"2015-12":10000}), .2, mbaPattern.clone())
+                Transition("Yes", CF({"2015-12":10000}), .5, mbaPattern.clone())
             ]))
         ])
 
-        solver = Solver(root, createMaxExpected(), PeriodNPV(0.1 / 12, pd.Period("2015-12", "M")))
+        solver = Solver(root, createMaxExpected(), PeriodNPV(10 / 100 / 12, pd.Period("2015-12", "M")))
         solver.solve()
 
-        showTree(root)
+        showTree(root, "${:,.2f}")
